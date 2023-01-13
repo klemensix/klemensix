00e7fda526e882b00e591f097daaff96daf50309
klemensix <93378384+klemensix@users.noreply.github.com>
2023-01-13 19:57:04 +0100

    Add files via upload

---
+++ 00000000000/znajdz.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styl.css">
+    <title>Kwiaty</title>
+</head>
+<body>
+    <header>
+        <h1>Moje kwiaty</h1>
+    </header>
+    <nav>
+        <h3>Kwiaty dla Ciebie!</h3>
+        <ol type="I" >
+            <li value="2"><a href="https://www.swiatkwiatow.pl/">Rozpoznaj kwiaty</a></li>
+            <li ><a href="znajdz.php">Znajdź kwiaciarnię</a></li>
+        </ol>
+        
+    </nav>
+    <main>
+        <table>
+            <tr>
+                <td>Untego</td>
+                <td>Marcello</td>
+                <td colspan='2'>Bugatti</td>
+            </tr>
+            <tr>
+                <td>Kastrio</td>
+                <td>Finciade</td>
+            </tr>
+        </table>
+    </main>
+    <footer>
+        Stronę opracował: Klemensix
+    </footer>
+</body>
+</html>
